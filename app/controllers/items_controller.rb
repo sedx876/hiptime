7cb58365082abd2b1e6c149c620c5395e4739f01
Sharon Watkins <swatkins876@gmail.com>
2019-12-13 16:13:18 -0500

    Edit and Destroy files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
 
+    before_action :find_item, only: [:show, :edit, :update, :destroy]
     def index 
         @items = Item.all.order("created_at DESC")
+    end
+
+    def show
+      
     end
     
     def new 
@@ -17,10 +22,29 @@ class ItemsController < ApplicationController
         end
     end
 
+    def edit 
+    end
+
+    def update
+        if @item.update(item_params)
+            redirect_to item_path(@item)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+      @item.destroy
+      redirect_to root_path
+    end
+
     private
 
     def item_params
       params.require(:item).permit(:title, :description)
     end
 
+    def find_item
+        @item = Item.find(params[:id])
+    end
 end
